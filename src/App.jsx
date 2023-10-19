cfe0001bf1cba22b3c01ee42110842e6cbf113eb
Patrick Rooney <patrickrooney09@gmail.com>
2023-10-19 14:44:06 -0400

    exercise 2.9 complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 function App() {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", display: "on" },
+    { name: "Ada Lovelace", number: "39-44-5323523", display: "on" },
+    { name: "Dan Abramov", number: "12-43-234345", display: "on" },
+    { name: "Mary Poppendieck", number: "39-23-6423122", display: "on" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
@@ -42,9 +46,29 @@ function App() {
     setNewNumber(event.target.value);
   };
 
+  const handleSearch = (event) => {
+    event.preventDefault();
+    setSearch(event.target.value);
+    const lowerCaseSearch = event.target.value.toLowerCase();
+    persons.map((currentPerson) => {
+      if (!currentPerson.name.toLowerCase().includes(lowerCaseSearch)) {
+        currentPerson.display = "off";
+      } else {
+        currentPerson.display = "on";
+      }
+    });
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with
+          <input value={search} onChange={handleSearch}></input>
+        </div>
+      </form>
+      <h2>Add A New</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -59,11 +83,13 @@ function App() {
       <h2>Numbers</h2>
 
       {persons.map((currentPerson) => {
-        return (
-          <div key={currentPerson.name}>
-            {currentPerson.name} {currentPerson.number}
-          </div>
-        );
+        if (currentPerson.display === "on") {
+          return (
+            <div key={currentPerson.name}>
+              {currentPerson.name} {currentPerson.number}
+            </div>
+          );
+        }
       })}
     </div>
   );
