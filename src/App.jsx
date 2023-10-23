5459c4c47dfce3e1a7aea0f14c2ecc9b7de8fc01
Patrick Rooney <patrickrooney09@gmail.com>
2023-10-23 11:16:59 -0400

    phone numbers and persons now stored in db.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./Filter";
 import PersonForm from "./PersonForm";
 import Numbers from "./Numbers";
+import axios from "axios";
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", display: "on" },
-    { name: "Ada Lovelace", number: "39-44-5323523", display: "on" },
-    { name: "Dan Abramov", number: "12-43-234345", display: "on" },
-    { name: "Mary Poppendieck", number: "39-23-6423122", display: "on" },
-  ]);
-
+  // const [persons, setPersons] = useState([
+  //   { name: "Arto Hellas", number: "040-123456", display: "on" },
+  //   { name: "Ada Lovelace", number: "39-44-5323523", display: "on" },
+  //   { name: "Dan Abramov", number: "12-43-234345", display: "on" },
+  //   { name: "Mary Poppendieck", number: "39-23-6423122", display: "on" },
+  // ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <div>
